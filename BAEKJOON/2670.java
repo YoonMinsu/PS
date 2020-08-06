fd5c6dbaad7145121d37b79594e6557172d3f671
MintFlavor <55343263+YoonMinsu@users.noreply.github.com>
2020-08-06 09:36:10 +0900

    연속최대부분곱

--- BAEKJOON/2670.java
+++ BAEKJOON/2670.java
@@ -30,3 +30,29 @@ public class Main {
         System.out.printf("%.3f", max);
     }
 }
+// dp version
+import java.io.*;
+
+
+public class Main {
+
+    public static void main( String[] args ) throws IOException {
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+
+        int N = Integer.parseInt( br.readLine() );
+        double[] arr = new double[ N + 1 ];
+
+        double max = 0;
+        for ( int i = 0; i < N; ++i ) {
+            arr[ i ] = Double.parseDouble( br.readLine() );
+            max = Math.max( max, arr[ i ] );
+        }
+
+        for ( int i = 1; i < N; ++i ) {
+            arr[ i ] = Math.max( arr[ i ],arr[ i ] * arr[ i - 1 ] );
+            max = Math.max( max, arr[ i ] );
+        }
+
+        System.out.printf( "%.3f", max);
+    }
+}
