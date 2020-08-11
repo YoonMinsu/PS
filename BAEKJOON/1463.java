644d44bc31b79706a9e4c13dcb0e0b7826b36a38
MintFlavor <55343263+YoonMinsu@users.noreply.github.com>
2020-08-11 15:11:31 +0900

    1로 만들기

---
+++ BAEKJOON/1463.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+
+        int x = Integer.parseInt( br.readLine() );
+
+        int[] dp = new int[ x + 1 ];
+
+        dp[ 0 ] = 0;
+        dp[ 1 ] = 0;
+
+        for ( int i = 2; i <= x; ++i ) {
+
+            dp[ i ] = dp[ i - 1 ] + 1;
+
+            if ( i % 2 == 0 ) {
+                dp[ i ] = Math.min( dp[ i ] , dp[ i / 2 ] + 1 );
+            }
+
+            if ( i % 3 == 0 ) {
+                dp[ i ] = Math.min( dp[ i ] , dp[ i / 3 ] + 1 );
+            }
+
+        } // end for
+
+        System.out.println( dp[ x ] );
+    }
+}
